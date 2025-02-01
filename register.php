6e5c93ffab74576a8bc2912df81d452c467c1ea7
BuddhimaDishantha <dishanthabuddhima@gmail.com>
2025-02-01 10:42:10 +0530

    Upadated Register.php as user can see Notfications

--- register.php
+++ register.php
@@ -13,28 +13,14 @@ include('database.php');
     <div class="form-container">
         <form action="register.php" method="post">
             <h3>register now</h3>
-            
-           <input type="text" name="name" required placeholder="enter your name" > 
-           <input type="email" name="email" required placeholder="enter your email" > 
-           <input type="password" name="password" required placeholder="enter your password" > 
-           <input type="password" name="cpassword" required placeholder="confrim your name" >
-            <select name="user_type">
-                <option value="user">user</option>
-                <option value="admin">admin</option>
-            </select>
-            <input type="submit"  name="submit" value="register now" class="form-btn">
-<p>already have an account?<a href="login.php">login now</a> </p>
 
-        </form>
-    </div>
-</body>
-</html>
-<?php
+ <?php
 if(isset($_POST["submit"])){
     $name=$_POST["name"];
     $email=$_POST["email"];
     $password=$_POST["password"];
     $cpassword=$_POST["cpassword"];
+   
     
     $sql="SELECT *FROM user_form WHERE email='$email'"; 
     $result=mysqli_query($conn,$sql);
@@ -59,3 +45,19 @@ if(isset($_POST["submit"])){
 
 }
 ?>
+            
+           <input type="text" name="name" required placeholder="enter your name" > 
+           <input type="email" name="email" required placeholder="enter your email" > 
+           <input type="password" name="password" required placeholder="enter your password" > 
+           <input type="password" name="cpassword" required placeholder="confrim your name" >
+            <select name="user_type">
+                <option value="user">user</option>
+                <option value="admin">admin</option>
+            </select>
+            <input type="submit"  name="submit" value="register now" class="form-btn">
+<p>already have an account?<a href="login.php">login now</a> </p>
+
+        </form>
+    </div>
+</body>
+</html>
